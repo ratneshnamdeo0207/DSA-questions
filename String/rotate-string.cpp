3a449044b6a1a61739eb5d763ff7a7b84d986438
ratneshnamdeo0207 <159109475+ratneshnamdeo0207@users.noreply.github.com>
2025-02-23 11:02:29 +0530

    Create rotate-string.cpp

---
+++ String/rotate-string.cpp
@@ -0,0 +1,42 @@
+//769 - https://leetcode.com/problems/rotate-string/
+//Optimized solution 
+bool rotateString(string s, string goal) {
+        if(s.size() != goal.size())
+        return false;
+        return (s+s).find(goal) != string::npos;
+    }
+
+class Solution {
+private:
+string rotate(string s)
+{
+    string temp= s;
+    int n = s.size();
+    for(int i = 0;i < n;i++)
+    {
+        temp[(i + 1)% n] = s[i];
+    }
+    
+    return temp;
+}
+public:
+   bool rotateString(string s, string goal) {
+      
+        if(goal.size() != s.size())
+        {
+            return false;
+        }
+        //Input: s = "acbde", goal = "cdeab"
+        if(s == goal)
+        return true;
+        string r = s;
+        for(int i = 0;i < s.size();i++)
+        {
+           r = rotate(r);
+            cout<<r<<endl;
+            if( r == goal)
+            return true;
+        }
+        return false;
+    }
+};
