394e7968e3db6f449f487c0883b17790e7792fe7
ratneshnamdeo0207 <159109475+ratneshnamdeo0207@users.noreply.github.com>
2025-06-01 10:38:42 +0530

    Create reversing-queue.cpp

---
+++ Queue/reversing-queue.cpp
@@ -0,0 +1,17 @@
+//https://www.naukri.com/code360/problems/reversing-queue_1170046?leftPanelTabValue=PROBLEM
+#include <bits/stdc++.h> 
+void reverse(queue < int > & q) {
+    // Write your code here.
+    stack<int> s;
+    while(!q.empty())
+    {
+        s.push(q.front());
+        q.pop();
+      
+    }
+    while(!s.empty())
+    {
+         q.push(s.top());
+        s.pop();
+    }
+}
